c4453e8773ab676b58a6698953c79c18d5c426a9
Melvin Lim <melvinlim@MacBook.local>
2018-05-19 17:42:01 -0700

    verify dates and step through them a day at a time

--- sim.py
+++ sim.py
@@ -1,2 +1,12 @@
 from marketdata import *
 md=MarketData('allData.pkl')
+data=md['all']
+dates=data.keys()
+dates.sort()
+for date in dates:
+	today=data[date]
+	for stock in today:
+		d=today[stock].pop('timestamp',None)
+		assert d==date
+	print today
+	raw_input()
