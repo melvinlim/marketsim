b850b4cd820ebb42389f17d65d8d5af578fdd463
Melvin Lim <melvinlim@MacBook.local>
2018-05-19 21:08:50 -0700

    disallow purchases if result is negative balance

--- account.py
+++ account.py
@@ -6,12 +6,15 @@ class Account():
 		self.funds=funds
 		self.stocks=dict()
 	def buy(self,amount,stock,price):
+		fundsAfter=self.funds-amount*float(price)
+		if fundsAfter<0:
+			return False
 		if stock not in self.stocks:
 			self.stocks[stock]=0
 		self.stocks[stock]+=100
-		self.funds-=amount*float(price)
+		self.funds=fundsAfter
 	def sell(self,amount,stock,price):
 		if stock not in self.stocks or self.stocks[stock]==0:
-			return 0
+			return False
 		self.stocks[stock]-=100
 		self.funds+=amount*float(price)
