476e6a28a7bddf1c68ec068bc28aa41993f49eef
Melvin Lim <melvinlim@MacBook.local>
2018-05-22 19:23:18 -0700

    remove subdirectory to convert to submodule

--- qlearn/README.md
+++
@@ -1,2 +0,0 @@
-# qlearn
-qlearning library
--- qlearn/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('qlearn',
-                    sources = ['main.cpp','player.cpp','qfunction.cpp','mynn/layer.cpp','mynn/net.cpp','mynn/idx.cpp'])
-
-setup (name = 'QLearn',
-       version = '1.0',
-       description = 'Q Learning package',
-       ext_modules = [module1])
